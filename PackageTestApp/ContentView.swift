8e66d51e6ab27b3616b7f84eabb4c7be6733cf1c
Mark DiFranco <mpdifranco@icloud.com>
2019-10-08 20:55:51 -0400

    Initial Commit

---
+++ PackageTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PackageTestApp
+//
+//  Created by Mark DiFranco on 2019-10-08.
+//  Copyright © 2019 Mark DiFranco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
