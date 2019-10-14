d870735c8ad15b4a693c30c1219d6b8c236f1f6a
Mark DiFranco <mpdifranco@icloud.com>
2019-10-14 13:36:12 -0400

    Link to PackageTest

--- PackageTestApp/AppDelegate.swift
+++ PackageTestApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import PackageTest
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        "It worked!".printToConsole()
+
         return true
     }
 
